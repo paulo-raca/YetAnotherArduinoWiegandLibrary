bd4e2726ba7faee182a51888822862b17e03f6d3
Paulo Costa <me@paulo.costa.nom.br>
2018-09-13 05:32:12 -0300

    Update examples to use new error callback

--- examples/pin_change_interrupts/pin_change_interrupts.ino
+++ examples/pin_change_interrupts/pin_change_interrupts.ino
@@ -8,8 +8,8 @@
 
 // These are the pins connected to the Wiegand D0 and D1 signals.
 // Ensure your board supports external Pin Change Interrupts on these pins
-#define PIN_D0 9
-#define PIN_D1 8
+#define PIN_D0 2
+#define PIN_D1 3
 
 // The object that handles the wiegand protocol
 Wiegand wiegand;
@@ -20,8 +20,9 @@ void setup() {
 
   //Install listeners and initialize Wiegand reader
   wiegand.onReceive(receivedData, "Card readed: ");
+  wiegand.onReceiveError(receivedDataError, "Card read error: ");
   wiegand.onStateChange(stateChanged, "State changed: ");
-  wiegand.begin(WIEGAND_LENGTH_AUTO);
+  wiegand.begin(Wiegand::LENGTH_ANY, true);
 
   //initialize pins as INPUT and attaches interruptions
   pinMode(PIN_D0, INPUT);
@@ -58,13 +59,31 @@ void stateChanged(bool plugged, const char* message) {
 // Notifies when a card was read.
 // Instead of a message, the seconds parameter can be anything you want -- Whatever you specify on `wiegand.onReceive()`
 void receivedData(uint8_t* data, uint8_t bits, const char* message) {
-    Serial.println(message);
-
+    Serial.print(message);
+    Serial.print(bits);
+    Serial.print("bits / ");
     //Print value in HEX
     uint8_t bytes = (bits+7)/8;
     for (int i=0; i<bytes; i++) {
         Serial.print(data[i] >> 4, 16);
         Serial.print(data[i] & 0xF, 16);
+    }
+    Serial.println();
+}
+
+// Notifies when an invalid transmission is detected
+void receivedDataError(Wiegand::DataError error, uint8_t* rawData, uint8_t rawBits, const char* message) {
+    Serial.print(message);
+    Serial.print(Wiegand::DataErrorStr(error));
+    Serial.print(" - Raw data: ");
+    Serial.print(rawBits);
+    Serial.print("bits / ");
+
+    //Print value in HEX
+    uint8_t bytes = (rawBits+7)/8;
+    for (int i=0; i<bytes; i++) {
+        Serial.print(rawData[i] >> 4, 16);
+        Serial.print(rawData[i] & 0xF, 16);
     }
     Serial.println();
 }
--- examples/polling/polling.ino
+++ examples/polling/polling.ino
@@ -5,8 +5,8 @@
 #include <Wiegand.h>
 
 // These are the pins connected to the Wiegand D0 and D1 signals.
-#define PIN_D0 18
-#define PIN_D1 19
+#define PIN_D0 2
+#define PIN_D1 3
 
 // The object that handles the wiegand protocol
 Wiegand wiegand;
@@ -17,8 +17,9 @@ void setup() {
 
   //Install listeners and initialize Wiegand reader
   wiegand.onReceive(receivedData, "Card readed: ");
+  wiegand.onReceiveError(receivedDataError, "Card read error: ");
   wiegand.onStateChange(stateChanged, "State changed: ");
-  wiegand.begin(WIEGAND_LENGTH_AUTO);
+  wiegand.begin(Wiegand::LENGTH_ANY, true);
 
   //initialize pins as INPUT
   pinMode(PIN_D0, INPUT);
@@ -46,12 +47,30 @@ void stateChanged(bool plugged, const char* message) {
 // Instead of a message, the seconds parameter can be anything you want -- Whatever you specify on `wiegand.onReceive()`
 void receivedData(uint8_t* data, uint8_t bits, const char* message) {
     Serial.print(message);
-
+    Serial.print(bits);
+    Serial.print("bits / ");
     //Print value in HEX
     uint8_t bytes = (bits+7)/8;
     for (int i=0; i<bytes; i++) {
         Serial.print(data[i] >> 4, 16);
         Serial.print(data[i] & 0xF, 16);
+    }
+    Serial.println();
+}
+
+// Notifies when an invalid transmission is detected
+void receivedDataError(Wiegand::DataError error, uint8_t* rawData, uint8_t rawBits, const char* message) {
+    Serial.print(message);
+    Serial.print(Wiegand::DataErrorStr(error));
+    Serial.print(" - Raw data: ");
+    Serial.print(rawBits);
+    Serial.print("bits / ");
+
+    //Print value in HEX
+    uint8_t bytes = (rawBits+7)/8;
+    for (int i=0; i<bytes; i++) {
+        Serial.print(rawData[i] >> 4, 16);
+        Serial.print(rawData[i] & 0xF, 16);
     }
     Serial.println();
 }
