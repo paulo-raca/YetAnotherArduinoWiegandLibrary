72241a325146aad452b36e953c3b3bb4363d4501
Paulo Costa <me@paulo.costa.nom.br>
2018-09-13 04:42:40 -0300

    Pretty radical rewrite.
    - A new callback can be used to detect and debug communication errors.
    - Message decoding can now be disabled, in case you need the raw message data or if you are using a weird format.
    - Message decoding now supports 4/8/26/32 bit modes
    - If the payload size is not multiple of 8 bits, it is padded on the "left" (MSB)
    
    This should take care of issue #4

--- src/Wiegand.cpp
+++ src/Wiegand.cpp
@@ -1,87 +1,22 @@
 #include <Wiegand.h>
 #include <Arduino.h>
 
-#define PIN_0                0x01
-#define PIN_1                0x02
-#define SETUP_READY          0x04
-#define SETUP_INITIALIZED    0x08
-#define FLAG_PARITY_LEFT     0x10
-#define FLAG_PARITY_RIGHT    0x20
-#define FLAG_LAST_BIT        0x40
-#define FLAG_INVALID         0x80
+#define PIN_0                        0x01
+#define PIN_1                        0x02
+#define DEVICE_CONNECTED             0x04
+#define DEVICE_INITIALIZED           0x08
 
-#define MASK_PINS            0x03
-#define MASK_SETUP           0x0C
-#define MASK_FLAGS           0xF0
+#define ERROR_TRANSMISSION           0x10
+#define ERROR_TOO_BIG                0x20
 
+#define MASK_PINS                    (PIN_0 | PIN_1)
+#define MASK_STATE                   0x0F
+#define MASK_ERRORS                  0xF0
 
-
-
-void Wiegand::begin(uint8_t bits) {
-    expected_bits = bits;
-
-    //Set state as "INVALID", so that data can only be received after a few millis in "ready" state
-    state &= MASK_PINS | SETUP_READY;
-    state |= SETUP_INITIALIZED | FLAG_INVALID;
-    timestamp = millis();
-}
-
-
-void Wiegand::end() {
-    state &= MASK_PINS | SETUP_READY;
-}
-
-
-void Wiegand::reset() {
-    bits=0;
-    state &= ~MASK_FLAGS;
-}
-
-
-// Returns if this device is initialized (with `begin()`) and a reader has been detected (Both D0 and D1 are pulled high)
-Wiegand::operator bool() {
-    return (state & (SETUP_READY|SETUP_INITIALIZED)) == (SETUP_READY|SETUP_INITIALIZED);
-}
-
-
-
-
-// If we have a complete payload on the buffer and parity checks out, sends a notification.
-void Wiegand::flushData() {
-    // If the frame has the expected size
-    if ( (bits == expected_bits) || ( ((expected_bits == WIEGAND_LENGTH_AUTO) && ( (bits == 26) || (bits == 34) ) ) ) ) {
-        // And it is valid: parity checks out, no overflow.
-        if ( !(state & FLAG_INVALID) && ((state & (FLAG_PARITY_LEFT | FLAG_PARITY_RIGHT)) == FLAG_PARITY_RIGHT) ) {
-            if (func_data) {
-                func_data(data, bits-2, func_data_param);
-            }
-        }
-    }
-}
-
-
-// Clean up state if no events happened for `WIEGAND_TIMEOUT` milliseconds.
-// This means sending out any complete message and reseting the state machine
-void Wiegand::flush() {
-    // Ignore if this device is disabled
-    if (!*this) {
-        return;
-    }
-
-    unsigned long elapsed = millis() - timestamp;
-    // Resets state if nothing happened in a few milliseconds
-    if (elapsed > WIEGAND_TIMEOUT) {
-        // Might have a pending data package, if expected_bits == WIEGAND_LENGTH_AUTO
-        flushData();
-        reset();
-    }
-}
-
-
-
-
-// Sets the value of the `i`-th payload bit
-inline void Wiegand::writeBit(uint8_t i, bool value) {
+/**
+ * Sets the value of the `i`-th data bit
+ */
+inline void writeBit(uint8_t* data, uint8_t i, bool value) {
     if (value) {
         data[i>>3] |=  (0x80 >> (i&7));
     } else {
@@ -89,67 +24,247 @@ inline void Wiegand::writeBit(uint8_t i, bool value) {
     }
 }
 
-
-// Reads the value of the `i`-th payload bit
-inline bool Wiegand::readBit(uint8_t i) {
+/**
+ * Reads the value of the `i`-th data bit
+ */
+inline bool readBit(uint8_t* data, uint8_t i) {
     return bool(data[i>>3] & (0x80 >> (i&7)));
 }
 
+/**
+ * Sign a subrange of data, shrinks and aligns the buffer to the right, inline
+ *
+ * returns the number of bits in the subrange
+ */
+inline uint8_t align_data(uint8_t* data, uint8_t start, uint8_t end) {
+    uint8_t aligned_data[Wiegand::MAX_BYTES];
+    uint8_t aligned_bits = end - start;
+    uint8_t aligned_bytes = (aligned_bits + 7)/8;
+    uint8_t aligned_offset = 8*aligned_bytes - aligned_bits;
 
-void Wiegand::addBit(bool value) {
-    //Skip if it is not initialized / Not ready.
-    if (!*this) {
-        return;
+    aligned_data[0] = 0;
+    for (int bit=0; bit<aligned_bits; bit++) {
+        writeBit(aligned_data, bit + aligned_offset, readBit(data, bit+start));
     }
-    //Skip if we have too much data
-    if (bits >= 8*WIEGAND_MAX_BYTES+2) {
-        state |= FLAG_INVALID;
+    for (int i=0; i<aligned_bytes; i++) {
+        data[i] = aligned_data[i];
     }
-    if (state & FLAG_INVALID) {
-        return;
-    }
+    return aligned_bits;
+}
 
-    // Special case for 1st bit
-    if (bits == 0) {
-        if (value) {
-            state |= FLAG_PARITY_LEFT;
-        }
 
-    } else {
-        // Store data bit
-        // Notice that we perform a delayed write, because we have to ignore the first and last bits (the parity bits)
-        if (bits >= 2) {
-            writeBit(bits-2, state & FLAG_LAST_BIT);
-        }
+/**
+ * Sets the device as "initialized" and resets it to wait a new message.
+ *
+ * If `expected_bits` is specified (usually 4, 8, 26 or 34), the data callback will
+ * be notified immedialy aftern the last bit is received.
+ *
+ * Otherwise (`expected_bits=LENGTH_ANY`), you will need to call `flush()`
+ * inside your main loop to receive notifications, and the end of the message will be
+ * triggered after a few ms without communication.
+ *
+ *
+ * if `decode_messages` is set, parity bits will be checked and removed
+ * during preprocessing, otherwise the raw message will be sent to the callback.
+ */
+void Wiegand::begin(uint8_t expected_bits, bool decode_messages) {
+    this->expected_bits = expected_bits;
+    this->decode_messages = decode_messages;
 
-        // Updates parity
-        if (value) {
-            state ^= FLAG_PARITY_RIGHT;
-        }
-        // As data grows, the bits that were considered part of the "Right" become part of the "Left".
-        // This chunk updates the parity flags accordingly
-        if (bits&1 && bits>=3) {
-            if (readBit((bits-3)/2)) {
-                state ^= FLAG_PARITY_LEFT | FLAG_PARITY_RIGHT;
-            }
-        }
-    }
-    bits++;
-    state = value ? (state | FLAG_LAST_BIT) : (state & ~FLAG_LAST_BIT);
+    //Set state as "INVALID", so that data can only be received after a few millis in "ready" state
+    bits=0;
+    timestamp = millis();
+    state = (state & MASK_STATE) | DEVICE_INITIALIZED | ERROR_TRANSMISSION;
+}
 
-    // If we know the number of bits, there is no need to wait for the timeout to send the data
-    if (expected_bits && (bits == expected_bits)) {
-        flushData();
-        state |= FLAG_INVALID;
+/**
+ * Sets the device as "not-initialized"
+ */
+void Wiegand::end() {
+    expected_bits = 0;
+
+    bits=0;
+    timestamp = millis();
+    state &= MASK_STATE & ~DEVICE_INITIALIZED;
+}
+
+
+/**
+ * Resets the state so that it awaits a new message.
+ *
+ * If the data pins aren't high, it sets the `ERROR_TRANSMISSION` flag
+ * to signal it is probably in the middle of a truncated message or something.
+ */
+void Wiegand::reset() {
+    bits=0;
+    state &= MASK_STATE;
+    //A transmission must start with D0=1, D1=1
+    if ((state & MASK_PINS) != MASK_PINS) {
+        state |= ERROR_TRANSMISSION;
     }
 }
 
 
+/**
+ * Returns if this device is initialized (with `begin()`) and a reader has been connected.
+ *
+ * A reader is considered connected when both D0 and D1 are high,
+ * and it is considered disconnected when both are low.
+ */
+Wiegand::operator bool() {
+    return (state & (DEVICE_CONNECTED|DEVICE_INITIALIZED)) == (DEVICE_CONNECTED|DEVICE_INITIALIZED);
+}
 
 
-// Updates the state of
+/**
+ * Verifies if the current buffer is valid and sends it to the data / error callbacks.
+ * If the buffer is invalid, it is discarded
+ */
+void Wiegand::flushData() {
+    //Ignore empty messages
+    if ((bits == 0) || (expected_bits == 0)) {
+        return;
+    }
+
+    //Check for pending errors
+    if (state & MASK_ERRORS) {
+        if (func_data_error) {
+            bits = align_data(data, 0, bits);
+            if (state & ERROR_TOO_BIG) {
+                func_data_error(DataError::SizeTooBig, data, bits, func_data_error_param);
+            } else {
+                func_data_error(DataError::Communication, data, bits, func_data_error_param);
+            }
+        }
+        return;
+    }
+
+    //Validate the message size
+    if ((expected_bits != bits) && (expected_bits != Wiegand::LENGTH_ANY)) {
+        if (func_data_error) {
+            bits = align_data(data, 0, bits);
+            func_data_error(DataError::SizeUnexpected, data, bits, func_data_error_param);
+        }
+        return;
+    }
+
+    //Decode the message
+    if (!decode_messages) {
+        if (func_data) {
+            bits = align_data(data, 0, bits);
+            func_data(data, bits, func_data_param);
+        }
+    } else {
+        //4-bit keycode: No check necessary
+        if ((bits == 4)) {
+            if (func_data) {
+                bits = align_data(data, 0, bits);
+                func_data(data, bits, func_data_param);
+            }
+
+        //8-bit keybode: UpperNibble = ~lowerNibble
+        } else if ((bits == 8)) {
+            uint8_t value = data[0] & 0xF;
+            if (data[0] == (value | ((0xF & ~value)<<4))) {
+                if (func_data) {
+                    func_data(&value, 4, func_data_param);
+                }
+            } else {
+                if (func_data_error) {
+                    bits = align_data(data, 0, bits);
+                    func_data_error(DataError::VerificationFailed, data, bits, func_data_error_param);
+                }
+            }
+
+        //26 or 34-bits: First and last bits are used for parity
+        } else if ((bits == 26) || (bits == 34)) {
+            //FIXME: The parity check doesn't seem to work for a 34-bit reader I have,
+            //but I suspect that the reader is non-complaint
+
+            boolean left_parity = false;
+            boolean right_parity = false;
+            for (int i=0; i<(bits+1)/2; i++) {
+                left_parity = (left_parity != readBit(data, i));
+            }
+            for (int i=bits/2; i<bits; i++) {
+                right_parity = (right_parity != readBit(data, i));
+            }
+
+            if (!left_parity && right_parity) {
+                if (func_data) {
+                    bits = align_data(data, 1, bits-1);
+                    func_data(data, bits, func_data_param);
+                }
+            } else {
+                if (func_data_error) {
+                    bits = align_data(data, 0, bits);
+                    func_data_error(DataError::VerificationFailed, data, bits, func_data_error_param);
+                }
+            }
+
+        } else {
+            if (func_data_error) {
+                bits = align_data(data, 0, bits);
+                func_data_error(DataError::DecodeFailed, data, bits, func_data_error_param);
+            }
+        }
+    }
+}
+
+
+/**
+ * Clean up state after `TIMEOUT` milliseconds without events
+ *
+ * This means sending out any pending message and calling `reset()`
+ */
+void Wiegand::flush() {
+    unsigned long elapsed = millis() - timestamp;
+    // Resets state if nothing happened in a few milliseconds
+    if (elapsed > TIMEOUT) {
+        // Might have a pending data package
+        flushData();
+        reset();
+    }
+}
+
+/**
+ * Immediately cleans up state, sending out pending messages and calling `reset()`
+ */
+void Wiegand::flushNow() {
+    flushData();
+    reset();
+}
+
+/**
+ * Adds a new bit to the payload
+ */
+void Wiegand::addBitInternal(bool value) {
+    //Skip if we have too much data
+    if (bits >= MAX_BITS) {
+        state |= ERROR_TOO_BIG;
+    } else {
+        writeBit(data, bits++, value);
+    }
+
+    // If we know the number of bits, there is no need to wait for the timeout to send the data
+    if (expected_bits > 0 && (bits == expected_bits)) {
+        flushData();
+        reset();
+    }
+}
+
+
+/**
+ * Updates the state of a pin.
+ *
+ * It will trigger adding bits to the payload, device connection/disconnection,
+ * dispatching the payload to the callback, etc
+ */
 void Wiegand::setPinState(uint8_t pin, bool pin_state) {
-    uint8_t pin_mask = pin? PIN_1 : PIN_0;
+    uint8_t pin_mask = pin ? PIN_1 : PIN_0;
+
+    flush();
+    timestamp = millis();
 
     //No change? Abort!
     if (bool(state & pin_mask) == pin_state) {
@@ -160,28 +275,31 @@ void Wiegand::setPinState(uint8_t pin, bool pin_state) {
         state &= ~pin_mask;
     }
 
-    //Flush old events
-    flush();
-    timestamp = millis();
-
-    //Both pins on - Databit received
-    if ( ( (state & MASK_PINS) == (PIN_0|PIN_1) ) ) {
+    //Both pins on: bit received
+    if ((state & MASK_PINS) == MASK_PINS) {
         //If the device wasn't ready before -- Enable it, and marks state as INVALID until is settles.
-        if (!(state & SETUP_READY)) {
-            state |= SETUP_READY|FLAG_INVALID;
+        if (state & DEVICE_CONNECTED) {
+            addBitInternal(pin);
+        } else {
+            //Device connection was detected right now!
+            //Set the device as connected, but unstable
+            state = (state & MASK_STATE) | DEVICE_CONNECTED | ERROR_TRANSMISSION;
             if (func_state) {
-              func_state(true, func_state_param);
+                func_state(true, func_state_param);
             }
         }
-        addBit(pin);
 
     //Both pins off - Device is unplugged
-    } else if ( !(state & MASK_PINS) ) {
-        if (state & SETUP_READY) {
-            state &= ~SETUP_READY;
-            state |= FLAG_INVALID;
+    } else if ((state & MASK_PINS) == 0) {
+        if (state & DEVICE_CONNECTED) {
+            //Flush truncated message, if any, and resets state
+            state |= ERROR_TRANSMISSION;
+            flushNow();
+
+            //Set the state as disconnected
+            state = (state & MASK_STATE & ~DEVICE_CONNECTED);
             if (func_state) {
-              func_state(false, func_state_param);
+                func_state(false, func_state_param);
             }
         }
     }
--- src/Wiegand.h
+++ src/Wiegand.h
@@ -1,69 +1,199 @@
-#define WIEGAND_LENGTH_AUTO     0
-
-#define WIEGAND_TIMEOUT       100
-#define WIEGAND_MAX_BYTES       4
-
 #include <stdint.h>
 
 class Wiegand {
+public:
+    /**
+     * Accepts messages of any size.
+     *
+     * Unfortunately, it also means waiting `TIMEOUT` after the last bit
+     * and calling `flush` to make sure that the message is finished.
+     */
+    static const uint8_t LENGTH_ANY = 0xFF;
+
+    /**
+     * 2ms seems to be the accepted interval between bits, but since it is very poorly
+     * standardized, so it's better to be on the safe side
+     */
+    static const uint8_t TIMEOUT = 25;
+
+    /**
+     * 34-bit is the maximum I've seen used for Wiegand
+     */
+    static const uint8_t MAX_BITS = 64;
+
+        /**
+     * 34-bit is the maximum I've seen used for Wiegand
+     */
+    static const uint8_t MAX_BYTES = ((MAX_BITS + 7)/8);
+
+    /**
+     * Possible communication errors sent to your `data_error_callback`
+     */
+    enum DataError { Communication, SizeTooBig, SizeUnexpected, DecodeFailed, VerificationFailed};
+
+    /**
+     * Gets the message associated with a `DataError`
+     */
+    static inline const char* DataErrorStr(DataError error) {
+        switch (error) {
+            case Communication:
+                return "Communication Error";
+            case SizeTooBig:
+                return "Message size too big";
+            case SizeUnexpected:
+                return "Message size unexpected";
+            case DecodeFailed:
+                return "Unsupported message format";
+            case VerificationFailed:
+                return "Message verification failed";
+            default:
+                return "Unknown";
+        }
+    }
+
+    typedef void (*data_callback)(uint8_t* data, uint8_t bits, void* param);
+    typedef void (*data_error_callback)(DataError error, uint8_t* rawdata, uint8_t bits, void* param);
+    typedef void (*state_callback)(bool plugged, void* param);
+
 private:
-  typedef void (*data_callback)(uint8_t* data, uint8_t bits, void* param);
-  typedef void (*state_callback)(bool plugged, void* param);
+    uint8_t expected_bits;
+    bool decode_messages;
+    uint8_t bits;
+    uint8_t state;
+    unsigned long timestamp;
+    uint8_t data[MAX_BYTES];
+    Wiegand::data_callback func_data;
+    Wiegand::data_error_callback func_data_error;
+    Wiegand::state_callback func_state;
+    void* func_data_param;
+    void* func_data_error_param;
+    void* func_state_param;
 
-  uint8_t expected_bits;
-  uint8_t bits;
-  uint8_t state;
-  unsigned long timestamp;
-  uint8_t data[WIEGAND_MAX_BYTES];
-  data_callback func_data;
-  state_callback func_state;
-  void* func_data_param;
-  void* func_state_param;
+    /**
+     * Adds a new bit to the payload
+     */
+    void addBitInternal(bool value);
 
-  inline void writeBit(uint8_t i, bool value);
-  inline bool readBit(uint8_t i);
-  void addBit(bool value);
-  void flushData();
-  void reset();
+    /**
+     * Verifies if the current buffer is valid and sends it to the data / error callbacks.
+     * If the buffer is invalid, it is discarded
+     */
+    void flushData();
 
 public:
-  // Start things up
-  // If the number of bits is specified (usually 26 or 34), the data Callback will be notified immedialy aftern the last bit.
-  // Otherwise, you will need to call `flush()` inside your main loop to receive notifications.
-  void begin(uint8_t bits=WIEGAND_LENGTH_AUTO);
-  // Stops it
-  void end();
+    /**
+    * Sets the device as "initialized" and resets it to wait a new message.
+    *
+    * If `expected_bits` is specified (usually 4, 8, 26 or 34), the data callback will
+    * be notified immedialy aftern the last bit is received.
+    *
+    * Otherwise (`expected_bits=LENGTH_ANY`), you will need to call `flush()`
+    * inside your main loop to receive notifications, and the end of the message will be
+    * triggered after a few ms without communication.
+    *
+    *
+    * if `decode_messages` is set, parity bits will be checked and removed
+    * during preprocessing, otherwise the raw message will be sent to the callback.
+    */
+    void begin(uint8_t expected_bits=LENGTH_ANY, bool decode_messages=true);
 
-  // Sends pending Data Received notifications.
-  // When using automatic length detection, this method has to be called frequently.
-  void flush();
+    /**
+    * Sets the device as "not-initialized"
+    */
+    void end();
 
-  // Checks if the class has started and a scanner is detected
-  operator bool();
+    /**
+     * Resets the state so that it awaits a new message.
+     *
+     * If the data pins aren't high, it sets the `ERROR_TRANSMISSION` flag
+     * to signal it is probably in the middle of a truncated message or something.
+     */
+    void reset();
 
-  //Attaches a Data Receive Callback. This will be called whenever a message has been received without errors.
-  template<typename T> void onReceive(void (*func)(uint8_t* data, uint8_t bits, T* param), T* param=nullptr) {
-    func_data = (data_callback)func;
-    func_data_param = (void*)param;
-  }
+    /**
+     * Returns if this device is initialized (with `begin()`) and a reader has been connected.
+     *
+     * A reader is considered connected when both D0 and D1 are high,
+     * and it is considered disconnected when both are low.
+     */
+    operator bool();
 
-  //Attaches a State Change Callback. This is called whenever a device is attached or dettached.
-  //If you have a dettachable device, add pull down resistors to both data lines, otherwise random noise will produce lots of bogus State Change notifications (and a few Data Received notifications)
-  template<typename T> void onStateChange(void (*func)(bool plugged, T* param), T* param=nullptr) {
-    func_state = (state_callback)func;
-    func_state_param = (void*)param;
-  }
+    /**
+     * Clean up state after `WIEGAND_TIMEOUT` milliseconds without events
+     *
+     * This means sending out any pending message and calling `reset()`
+     */
+    void flush();
 
-  //Notifies the library that the pin Data-`pin_value` has changed to `pin_state`
-  void setPinState(uint8_t pin, bool pin_state);
+    /**
+    * Immediately cleans up state, sending out pending messages and calling `reset()`
+    */
+    void flushNow();
 
-  //Notifies the library that the pin Data0 has changed to `pin_state`
-  inline void setPin0State(bool state) {
-    setPinState(0, state);
-  }
 
-  //Notifies the library that the pin Data1 has changed to `pin_state`
-  inline void setPin1State(bool state) {
-    setPinState(1, state);
-  }
+    /**
+     * Attaches a Data Receive Callback.
+     *
+     * This will be called whenever a message has been received without errors.
+     */
+    template<typename T> void onReceive(void (*func)(uint8_t* data, uint8_t bits, T* param), T* param=nullptr) {
+      func_data = (data_callback)func;
+      func_data_param = (void*)param;
+    }
+
+
+    /**
+     * Attaches a Data Transmission Error Callback.
+     *
+     * This will be called whenever a message has been received without errors.
+     */
+    template<typename T> void onReceiveError(void (*func)(DataError error, uint8_t* data, uint8_t bits, T* param), T* param=nullptr) {
+      func_data_error = (data_error_callback)func;
+      func_data_error_param = (void*)param;
+    }
+
+    /**
+     * Attaches a State Change Callback. This is called whenever a device is attached or dettached.
+     *
+     * If you have a dettachable device, add pull down resistors to both data lines, otherwise random
+     * noise will produce lots of bogus State Change notifications (and maybe a few Data Received notifications)
+     */
+    template<typename T> void onStateChange(void (*func)(bool plugged, T* param), T* param=nullptr) {
+      func_state = (state_callback)func;
+      func_state_param = (void*)param;
+    }
+
+    /**
+    * Updates the state of a pin.
+    *
+    * It will trigger adding bits to the payload, device connection/disconnection,
+    * dispatching the payload to the callback, etc
+    */
+    void setPinState(uint8_t pin, bool pin_state);
+
+    /**
+     * Notifies the library that the pin Data0 has changed to `pin_state`
+     */
+    inline void setPin0State(bool state) {
+      setPinState(0, state);
+    }
+
+    /**
+     * Notifies the library that the pin Data1 has changed to `pin_state`
+     */
+    inline void setPin1State(bool state) {
+      setPinState(1, state);
+    }
+
+    /**
+     * Receives a data bit.
+     *
+     * This is meant for testing only
+     */
+    inline void receivedBit(bool bitValue) {
+      setPinState(0, true);
+      setPinState(1, true);
+      setPinState(bitValue, false);
+      setPinState(bitValue, true);
+    }
 };
